064686f8ada0490955b2a56224704ed95e5429f5
Arpit Singh Verma <21bcs089@nith.ac.in>
2023-08-28 22:34:02 +0530

    1

---
+++ main.py
@@ -0,0 +1,13 @@
+def get_choices():
+    player_choice = "Arpit"
+    computer_choice = "paper"
+
+    return computer_choice
+
+
+def greeting():
+    return "Hi"
+
+
+response = greeting()
+print(response)

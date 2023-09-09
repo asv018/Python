631d3d6515c29a1b31407ff1260820add5b35433
Arpit Singh Verma <21bcs089@nith.ac.in>
2023-09-09 15:53:29 +0530

    Map , Filter , Reduce

--- mapFilterReduce.py
+++ mapFilterReduce.py
@@ -1,14 +1,48 @@
 # map() , filter() , reduce()
 
 
-numbers = [1, 2, 3]
+# numbers = [1, 2, 3]
 
 
-def double(a):
-    return a * 2
+# def double(a): return a * 2
 
 
-res = map(double, numbers)
+# res = map(double, numbers)
 
 
-print(list(res))
+# print(list(res))
+
+
+# numbers = [1, 2, 3]
+
+
+# def isEven(n):
+#     return n % 2 == 0
+
+
+# res = filter(lambda n: n % 2 == 0, numbers)
+
+# print(list(res))
+
+
+# expenses = [
+#     ('Dinner', 80),
+#     ('Coffee', 5),
+# ]
+
+# sum = 0
+# for expense in expenses:
+#     sum += expense[1]
+# print(sum)
+
+# this is the long way
+
+from functools import reduce
+
+expenses = [
+    ('Dinner', 80),
+    ('Coffee', 5),
+]
+
+sum = reduce(lambda acc, expense: acc + expense[1], expenses, 0)
+print(sum)
